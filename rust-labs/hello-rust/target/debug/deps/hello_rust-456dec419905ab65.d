c514d1a253a5c6f7925b7c4e1d85755b53b02b37
Rafael Borges <rborges@datawiden.net>
2022-11-10 07:56:12 -0300

    getting started with rust

---
+++ rust-labs/hello-rust/target/debug/deps/hello_rust-456dec419905ab65.d
@@ -0,0 +1,5 @@
+/home/rborges/source/repos/labs/rust-labs/hello-rust/target/debug/deps/hello_rust-456dec419905ab65: src/main.rs
+
+/home/rborges/source/repos/labs/rust-labs/hello-rust/target/debug/deps/hello_rust-456dec419905ab65.d: src/main.rs
+
+src/main.rs:
---
+++ rust-labs/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/rborges/source/repos/labs/rust-labs/hello-rust/target/debug/hello-rust: /home/rborges/source/repos/labs/rust-labs/hello-rust/src/main.rs
